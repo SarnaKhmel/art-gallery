df37524b9a9f760cba39e6f4f698dbb283143f25
sarnakhmel <OlSa@olsa.local>
2024-01-21 11:58:30 +0200

    changes

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./Banner.scss";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="banner">
+      <div className="banner__content">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex maxime
+        voluptatum laudantium culpa esse modi officiis recusandae ea neque,
+        voluptates aspernatur incidunt, deserunt at numquam optio autem enim.
+        Dolorem, tempora.
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import Layout from "../commonComponents/Layout";
-
+import Banner from "../Components/Banner/Banner";
+import HomeGallery from "../Components/HomeGallery/HomeGallery";
 const HomePage = () => {
   return (
     <>
-      <Layout>Home</Layout>
+      <Layout>
+        <Banner />
+        <HomeGallery />
+      </Layout>
     </>
   );
 };
